afde8e49bf4e431819eae0853476376e12315a99
saguaraz <saguaraz@catamarca>
2026-08-30 17:53:57 -0300

    Agredo A al nombre

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const estudiante = "Sebastián Guaraz";  
+const estudiante = "Sebastián A. Guaraz";  
 console.log(`Entorno preparado para ${estudiante}`);  
 console.log("¡Comienza el curso de Node.js!"); 
